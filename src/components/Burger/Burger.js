665a935393151c42234b2290f49e9ed2ebb5972e
Huynh Phong Luu <luuhuynh@me.com>
2020-06-28 22:58:46 +0700

    showed burger successfully

---
+++ src/components/Burger/Burger.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+import "./Burger.css";
+import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
+
+const burger = (props) => {
+  let transformedIngredients = Object.keys(props.ingredients)
+    .map((ingredient) => {
+      let temp = [];
+      for (let i = 0; i < props.ingredients[ingredient]; i++) {
+        console.log(ingredient, i);
+        temp.push(<BurgerIngredient key={ingredient + i} type={ingredient} />);
+      }
+      return [...temp];
+    })
+    .reduce((accumulator, currentValue) => {
+      return accumulator.concat(currentValue);
+    });
+
+  if (transformedIngredients.length === 0) {
+    transformedIngredients = <p>Please add some ingredients!</p>;
+  }
+
+  return (
+    <div className="Burger">
+      <BurgerIngredient type="bread-top" />
+      {transformedIngredients}
+      <BurgerIngredient type="bread-bottom" />
+    </div>
+  );
+};
+
+export default burger;
---
+++ src/containers/BurgerBuilder/BugerBuilder.js
@@ -0,0 +1,25 @@
+import React from "react";
+import Aux from "../../hoc/Aux";
+import Burger from "../../components/Burger/Burger";
+
+class BurgerBuilder extends React.Component {
+  state = {
+    ingredients: {
+      cheese: 0,
+      meat: 0,
+      salad: 0,
+      bacon: 0,
+    },
+  };
+
+  render() {
+    return (
+      <Aux>
+        <Burger ingredients={this.state.ingredients} />
+        <div>Build Controls</div>
+      </Aux>
+    );
+  }
+}
+
+export default BurgerBuilder;
