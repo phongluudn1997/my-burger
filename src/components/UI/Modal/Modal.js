e9718747c99a99b27cc6b504e94fe78e499ccbb8
Luu Huynh Phong <luu.huynh@fiisoft.com>
2020-07-06 08:27:11 +0700

    refactored: avoid rerendering Modal Component

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -3,19 +3,29 @@ import "./Modal.css";
 import Aux from "../../../hoc/Aux";
 import Backdrop from "../Backdrop/Backdrop";
 
-const modal = (props) => (
-  <Aux>
-    <Backdrop isShowed={props.isShowed} closeModal={props.closeModal} />
-    <div
-      style={{
-        transform: props.isShowed ? "translateY(0)" : "translateY(1)",
-        opacity: props.isShowed ? "1" : "0",
-      }}
-      className="Modal"
-    >
-      {props.children}
-    </div>
-  </Aux>
-);
+class Modal extends React.Component {
+  shouldComponentUpdate(nextProps, nextState) {
+    return nextProps.isShowed === this.props.isShowed;
+  }
+  render() {
+    return (
+      <Aux>
+        <Backdrop
+          isShowed={this.props.isShowed}
+          closeModal={this.props.closeModal}
+        />
+        <div
+          style={{
+            transform: this.props.isShowed ? "translateY(0)" : "translateY(1)",
+            opacity: this.props.isShowed ? "1" : "0",
+          }}
+          className="Modal"
+        >
+          {this.props.children}
+        </div>
+      </Aux>
+    );
+  }
+}
 
-export default modal;
+export default Modal;
