af684a139242150fc3d2b1a0b82be578f10593ec
Phong Luu <huynhpluu@gmail.com>
2020-10-25 10:14:00 +0700

    added: lazy loading

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import React, { Component } from "react";
+import React, { Component, Suspense, lazy } from "react";
 import { Route, Switch } from "react-router-dom";
 
 import Layout from "./hoc/Layout/Layout";
 import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
-import Checkout from "./containers/Checkout/Checkout";
-import Orders from "./containers/Orders/Orders";
-import Auth from "./containers/Auth/Auth";
 import Logout from "./containers/Auth/Logout";
+
+const Checkout = lazy(() => import("./containers/Checkout/Checkout"));
+const Auth = lazy(() => import("./containers/Auth/Auth"));
+const Orders = lazy(() => import("./containers/Orders/Orders"));
 
 class App extends Component {
   render() {
     return (
       <div>
         <Layout>
-          <Switch>
-            <Route path="/checkout" component={Checkout} />
-            <Route path="/orders" component={Orders} />
-            <Route path="/auth" component={Auth} />
-            <Route path="/logout" component={Logout} />
-            <Route path="/" exact component={BurgerBuilder} />
-          </Switch>
+          <Suspense fallback="Loading...">
+            <Switch>
+              <Route path="/checkout" component={Checkout} />
+              <Route path="/orders" component={Orders} />
+              <Route path="/auth" component={Auth} />
+              <Route path="/logout" component={Logout} />
+              <Route path="/" exact component={BurgerBuilder} />
+            </Switch>
+          </Suspense>
         </Layout>
       </div>
     );
